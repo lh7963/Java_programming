58ec393ba62df1b57d3cffb572f7f21bfeec2cf0
lh7963 <30308502+lh7963@users.noreply.github.com>
2018-07-30 22:17:13 +0800

    Add files via upload

--- 20180730.java
+++ 20180730.java
@@ -1,13 +1,1 @@
-
-public static void selectSort(int[] arr)
-{
-    for(int x=0;x<arr.length-1;x++)
-    {
-        if(arr[x]>arr[y])
-        {
-            int temp = arr[x];
-            arr[x] = arr[y];
-            arr[y] = temp;
-        }
-    }
-}
+adsf
